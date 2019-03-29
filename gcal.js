d66100a96ae8d7f3e88a98f1d5df76775462c447
dream12594 <tanawat.p@avalant.co.th>
2019-03-29 15:18:30 +0700

    1) change height month view mobile 2) add param in create calendar -isShowList -isShowDotEvent 3) fix bug

--- gcal.js
+++ gcal.js
@@ -1,5 +1,5 @@
 /*!
- * FullCalendar v0.0.0
+ * ava-FullCalendar v0.0.0
  * Docs & License: https://fullcalendar.io/
  * (c) 2018 Adam Shaw
  */
@@ -322,3 +322,4 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_3__;
 
 /******/ });
 });
+//# sourceMappingURL=gcal.js.map
